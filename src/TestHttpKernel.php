7c6297f8edaef11eced2d2b00f1d27277df2b2cb
Matt Glaman <nmd.matt@gmail.com>
2024-05-20 16:12:00 -0500

    Fix TerminableInterface compatibility in TestHttpKernel

--- src/TestHttpKernel.php
+++ src/TestHttpKernel.php
@@ -7,8 +7,9 @@ namespace mglaman\DrupalTestHelpers;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\TerminableInterface;
 
-final class TestHttpKernel implements HttpKernelInterface
+final class TestHttpKernel implements HttpKernelInterface, TerminableInterface
 {
     public function __construct(
         private readonly HttpKernelInterface $http_kernel
@@ -21,5 +22,12 @@ final class TestHttpKernel implements HttpKernelInterface
         bool $catch = true
     ): Response {
         return $this->http_kernel->handle($request, $type, false);
+    }
+
+    public function terminate(Request $request, Response $response): void
+    {
+        if ($this->http_kernel instanceof TerminableInterface) {
+            $this->http_kernel->terminate($request, $response);
+        }
     }
 }
